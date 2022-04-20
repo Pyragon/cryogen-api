e244c9ba09febd64f5224b1f2da69e3a5df5d727
Cody Thompson <eldo.imo.rs@hotmail.com>
2022-04-19 22:32:33 -0700

    Finish log section minus searching

--- src/utils/log.js
+++ src/utils/log.js
@@ -1,21 +1,57 @@
 const Cache = require('../utils/cache');
 
+const User = require('../models/User');
+
+const ObjectId = require('mongoose').Types.ObjectId;
+
 const idIsNPC = [
     'bob',
-    'bob_death'
+    'bob_death',
+    'npc_drop',
+    'shop',
 ];
 
 const idIsItem = [
+    'grand_exchange',
+    'npc_drop',
+    'pickup',
+    'player_owned_shop',
+    'shop',
+];
 
+const idIsUser = [
+    'dicing',
+    'duelling',
+    'grand_exchange',
+    'pickup',
+    'player_owned_shop',
+    'pvp',
+    'trade'
 ];
 
 module.exports = async function toRealValues(log) {
     log = {...log._doc };
     if (idIsNPC.includes(log.type))
         log.npc = await Cache.getNPCDefinitions(log.id);
-    else if (idIsItem.includes(log.type))
-        log.item = await Cache.getItemDefinitions(log.id);
+    if (idIsItem.includes(log.type))
+        log.extra.item.defs = await Cache.getItemDefinitions(log.extra.item.id);
+    if (idIsUser.includes(log.type)) {
+        try {
+            if (!ObjectId.isValid(log.id)) {
+                console.error('Invalid id: ' + log.id);
+                return log;
+            }
+            let user = await User.findById(log.id);
+            if (!user) {
+                console.error('Unable to find user');
+                return log;
+            }
 
+            log.user2 = user;
+        } catch (error) {
+            console.error(error);
+        }
+    }
     if (log.items)
         log.items = await Promise.all(log.items.map(async item => {
             let defs = await Cache.getItemDefinitions(item.id);
@@ -27,5 +63,27 @@ module.exports = async function toRealValues(log) {
             let defs = await Cache.getItemDefinitions(item.id);
             return {...item, defs };
         }));
+
+    if (log.type === 'death') {
+        if (log.extra.source.type === 'npc')
+            log.extra.source.defs = await Cache.getNPCDefinitions(log.id);
+        else if (log.extra.source.type === 'player') {
+            try {
+                if (!ObjectId.isValid(log.id)) {
+                    console.error('Invalid id: ' + log.id);
+                    return log;
+                }
+                let user = await User.findById(log.id);
+                if (!user) {
+                    console.error('Unable to find user');
+                    return log;
+                }
+
+                log.extra.source.player = user;
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    }
     return log;
 }
