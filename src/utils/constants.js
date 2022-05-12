ffdac7774479e41cae3b01644f6c60d2e55eb35b
Cody Thompson <eldo.imo.rs@hotmail.com>
2022-05-11 20:09:58 -0700

    Edit the default permissions id

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,9 +5,10 @@ module.exports = {
     CHATBOX_MUTED_USERGROUP: '61e7863d2fcb27a6a9744d54',
     REGULAR_USERGROUP: '61d53c02db49b2e1749bb34a',
     ADMIN_USERGROUP: '61d53bc06e69950afc61cc9b',
-    DEFAULT_PERMISSIONS: '61dfb50b7965a7abf4d992ea',
+    DEFAULT_PERMISSIONS: '627c6c00802f439500e295c0',
     createDefaultPermissions() {
         return new Permissions({
+            name: 'Created Default Permissions',
             canSee: [-1],
             canReply: [-2],
             canEdit: [-3],
